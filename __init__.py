e8a5f7202b527b7f3c013cfcab1eb6c3383e79ff
Michael Piazza <michael.piazza.mp@gmail.com>
2024-05-29 01:52:06 -0400

    refactors, update note on review

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from typing import List
-from aqt import gui_hooks, editor
+from aqt import gui_hooks, editor, mw
+from anki.cards import Card
 import requests
 
 # TODO: sort imports...
@@ -11,38 +12,70 @@ import requests
 
 
 
-def on_editor(buttons: List[str], e: editor.Editor):
-    def fn(editor: editor.Editor):
-        note = editor.note
-        expression = note["Expression"]
+# Create an OpenAPI Client
+class OpenAIClient:
+    def __init__(self, api_key):
+        self.api_key = api_key
 
+    def get_chat_response(self, prompt: str):
         r = requests.post(
             "https://api.openai.com/v1/chat/completions",
             headers={
-                "Authorization": f"Bearer {OPEN_AI_KEY}",
+                "Authorization": f"Bearer {self.api_key}",
             },
-            json={
-              "model": "gpt-4",
-              "messages": [
-                {
-                    "role": "user",
-                    "content": f"You are to provide an example sentence in Japanese for the word {expression}. Use only simple (N5) vocab and grammar.  Respond only with the Japanese example sentence followed by the english translation in paranthesis, nothing else.",
-                }
-              ]
-            }
+            json={"model": "gpt-4", "messages": [{"role": "user", "content": prompt}]},
         )
 
         resp = r.json()
         msg = resp["choices"][0]["message"]["content"]
-        print(msg)
+        return msg
+
+
+client = OpenAIClient(OPEN_AI_KEY)
+
+def make_prompt(expression: str):
+    return f"You are to provide an example sentence in Japanese for the word {expression}. Use only simple (N5) vocab and grammar.  Respond only with the Japanese example sentence followed by the english translation in paranthesis, nothing else."
+
+def on_editor(buttons: List[str], e: editor.Editor):
+    def fn(editor: editor.Editor):
+        note = editor.note
+        if not note:
+            print("Error: no note found")
+            return
+
+        expression = note["Expression"]
+
+        msg = client.get_chat_response(
+            make_prompt(expression)
+        )
+
         note["Example"] = msg
         editor.loadNote()
-
-
 
     button = e.addButton(cmd="Fill out stuff", func=fn, icon="!")
     buttons.append(button)
 
+def on_review(card: Card):
+    print("Reviewing...")
+    note = card.note()
+    example = note["Example"]
+    if not example:
+        example = client.get_chat_response(
+            make_prompt(note["Expression"])
+        )
+        note["Example"] = example
+        note["ExampleTTS"] = ""
+
+        if not mw:
+            print("Error: mw not found")
+            return
+
+        mw.col.update_note(note)
+        card.load()
+        print("Updated example sentence")
+    else:
+        print("Example sentence already exists")
 
 
 gui_hooks.editor_did_init_buttons.append(on_editor)
+gui_hooks.reviewer_did_show_question.append(on_review)
