9809bb22075a0870a449c5a3764e25eab592d416
Michael Piazza <michael.piazza.mp@gmail.com>
2025-08-14 15:39:48 +0900

    Suppress Sentry errors

--- src/sentry.py
+++ src/sentry.py
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with Smart Notes.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+# pyright: reportPrivateUsage=false
+
 import logging
 import os
 import sys
